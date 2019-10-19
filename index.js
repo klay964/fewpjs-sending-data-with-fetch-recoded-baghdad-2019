ba71d100c69609960737b1d1faa49b2c58a2f6ce
Mohammed Ali <dody200039@gmail.com>
2019-10-19 15:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+function submitData(name,email){
+  const requestOptions={
+    headers:{
+      'ConentTtype':'application/json',
+      'Accept':'application/json'
+    },
+    method:'post',
+    body:JSON.stringify({name:name,email:email})
+  }
+  return fetch(URL,requestOptions)
+}
+submitData("john","john@invisible.com").then(res =>{return res.json()})
+.then (json => {let elem=document.createElement("div")
+console.log(json.id)
+elem.innerHtml=json.id
+  document.body.appendChild(elem)}.catch(err=>{document.body.innerHTML=err.message})
